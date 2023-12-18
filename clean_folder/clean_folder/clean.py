f5b07eead60ab46418651c3ee1794dbdb080f053
Sylthee <dziekan.dawid@wp.pl>
2023-12-18 20:26:46 +0100

    Update clean.py
    literówka :)

--- clean_folder/clean_folder/clean.py
+++ clean_folder/clean_folder/clean.py
@@ -17,7 +17,7 @@ ARCHIVES_EXTENSIONS = ['ZIP', 'GZ', 'TAR']
 
 all_existing_extentions = set()
 
-unrecognize_extensions = set()
+unrecognized_extensions = set()
 
 def normalize(some_string):  
     result = ""
@@ -89,7 +89,7 @@ def process_folder(folder_path):
                 all_existing_extentions.add(extension)
                 archive_folder_and_move(root, file, "archiwa")
             else:
-                unrecognize_extensions.add(extension)
+                unrecognized_extensions.add(extension)
                 continue
                 
         for dir in dirs:
@@ -100,7 +100,7 @@ def process_folder(folder_path):
             else:
                 os.rename(dir_path ,new_dir_path)
     print(all_existing_extentions)
-    print(unrecognize_extensions)
+    print(unrecognized_extensions)
 
 def main():
     if len(sys.argv) == 2:
@@ -108,7 +108,6 @@ def main():
         process_folder(folder_to_sort)
     else:
         sys.exit(1)
-
 
 if __name__ == "__main__":
     main()
